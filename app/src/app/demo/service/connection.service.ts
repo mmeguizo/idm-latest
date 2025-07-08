c921ffbcc597850dc01944d90e17b743d1a65ab2
Mark Oliver Meguizo <mark.meguizo@gmail.com>
2025-07-08 16:54:18 +0800

    tester

---
+++ app/src/app/demo/service/connection.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class ConnectionService {
+    //ngrok development
+    // public domain: string = 'https://unduly-enjoyed-parrot.ngrok-free.app/';
+
+    //localhost dev
+    public domain: string = 'http://localhost:3002';
+
+    // if deployed online with hosting
+    // public domain: string = 'https://idm-backend.chmsu.edu.ph';
+    //
+    // if deployed online without hosting
+    // public domain: string = '';
+}
